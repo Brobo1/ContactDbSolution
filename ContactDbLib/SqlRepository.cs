1eff74996959a5952c76bb77382f3947b85d32a5
oogaboogaCat <Arne2812@outlook.com>
2023-11-27 14:46:43 +0100

    Lagd sqlrepo og gjort endringer i den

---
+++ ContactDbLib/SqlRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactDbLib
+{
+    static public class SqlRepository
+    {
+        private const string _connectionString =
+            @"Server = (localdb)\MSSQLLocalDB; " +
+            "Database = World; " +
+            "Integrated Security = true";
+
+        static public int CreateContact(string ssn, string firstName, string lastName) 
+        {
+        
+        }
+   
+        //Contact? ReadContact(int id)
+
+
+        //List<Contact> ReadAllContacts()
+
+        //bool DeleteContact(int id)
+        //bool UpdateContact(int id, string ssn, string firstName, string lastName)
+
+
+
+    }
+}
