bfdad9e4655195a62fed6a9f13f73dcc9c96d86b
oogaboogaCat <Arne2812@outlook.com>
2023-11-27 14:59:14 +0100

    Fortsettet skal pushe

--- ContactDbLib/SqlRepository.cs
+++ ContactDbLib/SqlRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace ContactDbLib
 {
@@ -15,15 +16,30 @@ namespace ContactDbLib
 
         static public int CreateContact(string ssn, string firstName, string lastName) 
         {
+            Contact newContact = new(ssn, firstName, lastName);
+
+            return 123;
         
         }
-   
-        //Contact? ReadContact(int id)
+        static public void ConnectionMethod()
+        {
+            using SqlConnection Connect = new(_connectionString);
+            SqlCommand command = Connect.CreateCommand();
+            command.CommandText = 
+                "SELECT Id,Ssn,FirstName,LastName \n" +
+                "FROM Contact \n" +
+                "WHERE "
 
+
+
+
+        }
+        //Contact? ReadContact(int id)
 
         //List<Contact> ReadAllContacts()
 
         //bool DeleteContact(int id)
+
         //bool UpdateContact(int id, string ssn, string firstName, string lastName)
 
 
