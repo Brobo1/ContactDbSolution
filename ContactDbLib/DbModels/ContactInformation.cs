f7111302b00249dcfd73aa936b307fae5842b537
brobo1 <armanahz95@gmail.com>
2023-11-28 15:39:21 +0100

    Created ContactInformation.cs

---
+++ ContactDbLib/DbModels/ContactInformation.cs
@@ -0,0 +1,19 @@
+﻿namespace ContactDbLib.DbModels;
+
+public class ContactInformation {
+	public int    Id              { get; set; }
+	public string Info            { get; set; }
+	public string AddsReservation { get; set; }
+	public int    ContactFk       { get; set; }
+
+	public ContactInformation(int id, string info, string addsReservation, int contactFk) {
+		Id              = id;
+		Info            = info;
+		AddsReservation = addsReservation;
+		ContactFk       = contactFk;
+	}
+
+	public override string ToString() {
+		return $"{Info} {AddsReservation}";
+	}
+}
