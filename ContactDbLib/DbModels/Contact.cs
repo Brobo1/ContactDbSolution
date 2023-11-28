c6d23f9db9a5145e3d939605c693b63837bdc64e
oogaboogaCat <Arne2812@outlook.com>
2023-11-28 11:13:19 +0100

    Lagd id field på contact

--- ContactDbLib/DbModels/Contact.cs
+++ ContactDbLib/DbModels/Contact.cs
@@ -2,6 +2,7 @@
 {
     public class Contact
     {
+        public int Id { get; set; }
         public string SSN { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
