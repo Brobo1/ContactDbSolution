3d7ffa9512930f20fdc3277a4a422c6f325732ec
brobo1 <armanahz95@gmail.com>
2023-11-28 14:52:39 +0100

    Address table in ContactDb

--- ContactDbApp/Program.cs
+++ ContactDbApp/Program.cs
@@ -7,31 +7,28 @@ using ContactDbLib;
 using ContactDbLib.DbModels;
 using Microsoft.Data.SqlClient;
 
-namespace ContactDbApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //SqlRepository.UpdateContact(1,  "Arne", "Boardman");
-            //SqlRepository.UpdateContact(3, "Arman", "Armando");
-            //SqlRepository.DeleteContact(1);
-            //   Console.WriteLine("input  after");
-            //  Console.WriteLine(SqlRepository.CreateContact("54173233124", "Mr.test1", "testinge1"));
+namespace ContactDbApp {
+	internal class Program {
+		static void Main(string[] args) {
+			//SqlRepository.UpdateContact(1,  "Arne", "Boardman");
+			//SqlRepository.UpdateContact(3, "Arman", "Armando");
+			//SqlRepository.DeleteContact(1);
+			//   Console.WriteLine("input  after");
+			//  Console.WriteLine(SqlRepository.CreateContact("54173233124", "Mr.test1", "testinge1"));
 
-            //   var all = SqlRepository.ReadAllContacts();
-            //  foreach (var item in all) {
-            //     Console.WriteLine(item.ToString());
-            //  }
+			//   var all = SqlRepository.ReadAllContacts();
+			//  foreach (var item in all) {
+			//     Console.WriteLine(item.ToString());
+			//  }
 
-            //var contact = SqlRepository.ReadContact(2);
-            //Console.WriteLine(contact?.ToString());
-            Contact cont = new("54176413124", "Barne", "marne");
-            SqlRepository.UpdateContact(cont);
+			//var contact = SqlRepository.ReadContact(2);
+			//Console.WriteLine(contact?.ToString());
+			Contact cont = new("54176413124", "Barne", "marne");
+			SqlRepository.UpdateContact(cont);
 
-            Contact contact = new("10101010101", "Jethro", "Tull");
-            contact = SqlRepository.CreateContact(contact);
-            Console.WriteLine($"ID {contact.Id}, {contact}");
-        }
-    }
+			Contact contact = new("10101010101", "Jethro", "Tull");
+			contact = SqlRepository.CreateContact(contact);
+			Console.WriteLine($"ID {contact.Id}, {contact}");
+		}
+	}
 }
--- InsertInto.sql
+++ InsertInto.sql
@@ -3,19 +3,33 @@
 --personnummer), FirstName og LastName. Sørg for at hvert element i SSN feltet er unikt. 
 --Ingen felt skal tillate nullverdier. Alt unntatt ID (løpenummer) skal være varchar.
 --Lagre SQL for dette i et CreateTable.sql script!
-Create database ContactDb; 
-CREATE TABLE Contact(
-Id INT IDENTITY(1,1),
-Ssn VARCHAR(128) UNIQUE NOT NULL,
-FirstName VARCHAR(128) NOT NULL,
-LastName VARCHAR(128) NOT NULL,
-PRIMARY KEY(Id)
-);
+--Create database ContactDb;
+--CREATE TABLE Contact
+--(
+--    Id        INT IDENTITY (1,1),
+--    Ssn       VARCHAR(128) UNIQUE NOT NULL,
+--    FirstName VARCHAR(128)        NOT NULL,
+--    LastName  VARCHAR(128)        NOT NULL,
+--    PRIMARY KEY (Id)
+--);
+
+create table Address
+(
+    Id     INT IDENTITY (1,1),
+    Street VARCHAR(128) NOT NULL,
+    City   VARCHAR(128) NOT NULL,
+    Zip    VARCHAR(128) NOT NULL,
+    UNIQUE (Street, City, Zip),
+    PRIMARY KEY (Id)
+)
 
 --Legg inn minst tre rader i tabellen.
 
-INSERT INTO Contact(Ssn,FirstName,LastName)
-VALUES(28120123123,'Arne','Bretting'),(9876512345,'Ole Tobias','Badstue'),(2225558834,'Arman','Armani'),(435685336,'Rene','Pathfinder')
+--INSERT INTO Contact(Ssn, FirstName, LastName)
+--VALUES (28120123123, 'Arne', 'Bretting'),
+--       (9876512345, 'Ole Tobias', 'Badstue'),
+--       (2225558834, 'Arman', 'Armani'),
+--       (435685336, 'Rene', 'Pathfinder')
 
 --Lagre SQL for dette i et InsertInto.sql script!
 
