b720d19bc785274fb0add85e4b32a9087d55ffdf
brobo1 <armanahz95@gmail.com>
2023-11-28 15:05:57 +0100

    Added Address class

---
+++ ContactDbLib/DbModels/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactDbLib.DbModels; 
+
+public class Address {
+	public int    Id     { get; set; }
+	public string Street { get; set; }
+	public string City   { get; set; }
+	public string Zip    { get; set; }
+
+	public Address(string street, string city, string zip) {
+		Street = street;
+		City   = city;
+		Zip    = zip;
+	}
+	
+	
+}
